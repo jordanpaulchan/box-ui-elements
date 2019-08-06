a82fcdeecb0ef63053eddcc87b789079a49317a9
jordanchan <jordanchan@box.com>
2019-08-06 14:09:28 -0700

    feat(storybook): add different viewports

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,3 +4,4 @@ import '@storybook/addon-links/register';
 import '@storybook/addon-notes/register-panel';
 import '@storybook/addon-a11y/register';
 import 'storybook-addon-intl/register';
+import '@storybook/addon-viewport/register';
